ba7f3108c328f1b3f457d89b21ffd23b773a3747
Aruna Murugesan <arunax777@gmail.com>
2024-12-10 11:28:24 -0600

    Updated Navbar

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { useLocation } from "react-router-dom";
 
 const Navbar = ({ setShowOffersModal, setMeetingsEvents, setExplorePage }) => {
+  const location = useLocation();
+  const isAuthPage =
+    location.pathname === "/login" || location.pathname === "/register";
+
   return (
     <nav className="bg-transparent py-4 border-b-2 border-gray-00">
       <div className="container mx-auto flex justify-between items-center">
@@ -13,50 +18,56 @@ const Navbar = ({ setShowOffersModal, setMeetingsEvents, setExplorePage }) => {
             Serene Stays
           </h1>
         </div>
-        <div className="flex space-x-4 mb-4">
-          <Link
-            to="/login"
-            className="px-4 py-2 bg-white text-blue-900 rounded-lg font-semibold hover:bg-gray-800 hover:text-white transition font-sans"
-          >
-            Sign in
-          </Link>
-          <Link
-            to="/register"
-            className="px-4 py-2 bg-white text-blue-900 rounded-lg font-semibold hover:bg-gray-800 hover:text-white transition font-sans"
-          >
-            Register
-          </Link>
-        </div>
+        {/* Sign In and Register links */}
+        {!isAuthPage && (
+          <div className="flex space-x-4 mb-4">
+            <Link
+              to="/login"
+              className="px-4 py-2 bg-white text-blue-900 rounded-lg font-semibold hover:bg-gray-800 hover:text-white transition font-sans"
+            >
+              Sign in
+            </Link>
+            <Link
+              to="/register"
+              className="px-4 py-2 bg-white text-blue-900 rounded-lg font-semibold hover:bg-gray-800 hover:text-white transition font-sans"
+            >
+              Register
+            </Link>
+          </div>
+        )}
       </div>
 
-      <div className="bg-transparent py-1 mt-1">
-        <div className="container mx-auto flex justify-center space-x-12 text-xl ">
-          <button
-            onClick={() => setExplorePage(true)}
-            className="px-4 py-1 bg-transparent text-white rounded-lg font-semibold hover:bg-gray-700 hover:text-white transition font-sans"
-          >
-            Explore
-          </button>
-          <button
-            onClick={() => setMeetingsEvents(true)}
-            className="px-4 py-1 bg-transparent text-white rounded-lg font-semibold hover:bg-gray-700 hover:text-white transition font-sans"
-          >
-            Meetings & Events
-          </button>
-          <button
-            onClick={() => setShowOffersModal(true)}
-            className="px-4 py-1 bg-transparent text-white rounded-lg font-semibold hover:bg-gray-700 hover:text-white transition font-sans"
-          >
-            Offers
-          </button>
-          <Link
-            to="/my-trips"
-            className="px-4 py-1 bg-transparent text-white rounded-lg font-semibold hover:bg-gray-700 hover:text-white transition font-sans"
-          >
-            My Trips
-          </Link>
+      {/* Navbar buttons */}
+      {!isAuthPage && (
+        <div className="bg-transparent py-1 mt-1">
+          <div className="container mx-auto flex justify-center space-x-12 text-xl">
+            <button
+              onClick={() => setExplorePage(true)}
+              className="px-4 py-1 bg-transparent text-white rounded-lg font-semibold hover:bg-gray-700 hover:text-white transition font-sans"
+            >
+              Explore
+            </button>
+            <button
+              onClick={() => setMeetingsEvents(true)}
+              className="px-4 py-1 bg-transparent text-white rounded-lg font-semibold hover:bg-gray-700 hover:text-white transition font-sans"
+            >
+              Meetings & Events
+            </button>
+            <button
+              onClick={() => setShowOffersModal(true)}
+              className="px-4 py-1 bg-transparent text-white rounded-lg font-semibold hover:bg-gray-700 hover:text-white transition font-sans"
+            >
+              Offers
+            </button>
+            <Link
+              to="/my-trips"
+              className="px-4 py-1 bg-transparent text-white rounded-lg font-semibold hover:bg-gray-700 hover:text-white transition font-sans"
+            >
+              My Trips
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
     </nav>
   );
 };
