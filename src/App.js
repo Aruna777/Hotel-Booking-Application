d89888a395151d2ae4f7b76b9d232a63901d41cf
Aruna Murugesan <arunax777@gmail.com>
2024-12-13 08:23:31 -0600

    Seperated route definitions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./navbar/Login";
-import Register from "./navbar/Register";
 import Navbar from "./navbar/Navbar";
-import Home from "./Home/Home";
+
 import backgroundImage from "./assets/image.jpg";
 import OffersModal from "./navbar/OffersModal";
 import MeetingsEvents from "./navbar/MeetingsEvents";
 import ExplorePage from "./navbar/ExplorePage";
 import MyTripsPage from "./navbar/MyTripsPage";
-import Apartments from "./Home/properties/Apartments";
-import Cabins from "./Home/properties/Cabins";
-import HotelsPage from "./Home/properties/HotelsPage";
-import Farmstays from "./Home/properties/FarmStays";
-import Resorts from "./Home/properties/Resorts";
-import Villas from "./Home/properties/Villas";
-import GuestHouses from "./Home/properties/GuestHouses";
-import Motels from "./Home/properties/Motels";
-import LuxuryTents from "./Home/properties/LuxuruyTents";
-import Newyork from "./Home/destinations/Newyork";
-import Miami from "./Home/destinations/Miami";
-import Lasvegas from "./Home/destinations/Lasvegas";
-import Losangeles from "./Home/destinations/Losangeles";
-import Chicago from "./Home/destinations/Chicago";
+import { routes } from "./Home/RoutesConfig";
 
 const App = () => {
   const [showOffersModal, setShowOffersModal] = useState(false);
@@ -49,23 +34,10 @@ const App = () => {
           setMyTripsPage={setMyTripsPage}
         />
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/hotelsPage" element={<HotelsPage />} />
-          <Route path="/apartments" element={<Apartments />} />
-          <Route path="/resorts" element={<Resorts />} />
-          <Route path="/cabins" element={<Cabins />} />
-          <Route path="/villas" element={<Villas />} />
-          <Route path="/motels" element={<Motels />} />
-          <Route path="/guesthouses" element={<GuestHouses />} />
-          <Route path="/Luxurytents" element={<LuxuryTents />} />
-          <Route path="/farmstays" element={<Farmstays />} />
-          <Route path="/nyc" element={<Newyork />} />
-          <Route path="/miami" element={<Miami />} />
-          <Route path="/lasvegas" element={<Lasvegas />} />
-          <Route path="/losangeles" element={<Losangeles />} />
-          <Route path="/chicago" element={<Chicago />} />
+          {" "}
+          {routes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
         </Routes>
 
         {showOffersModal && (
---
+++ src/Home/RoutesConfig.js
@@ -0,0 +1,38 @@
+import Login from "../navbar/Login";
+import Register from "../navbar/Register";
+import Home from "./Home";
+import Chicago from "./destinations/Chicago";
+import Lasvegas from "./destinations/Lasvegas";
+import Losangeles from "./destinations/Losangeles";
+import Miami from "./destinations/Miami";
+import Newyork from "./destinations/Newyork";
+
+import Apartments from "./properties/Apartments";
+import Cabins from "./properties/Cabins";
+import Farmstays from "./properties/FarmStays";
+import GuestHouses from "./properties/GuestHouses";
+import HotelsPage from "./properties/HotelsPage";
+import LuxuryTents from "./properties/LuxuryTents";
+import Motels from "./properties/Motels";
+import Resorts from "./properties/Resorts";
+import Villas from "./properties/Villas";
+
+export const routes = [
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "/", element: <Home /> },
+  { path: "/hotelsPage", element: <HotelsPage /> },
+  { path: "/apartments", element: <Apartments /> },
+  { path: "/resorts", element: <Resorts /> },
+  { path: "/cabins", element: <Cabins /> },
+  { path: "/villas", element: <Villas /> },
+  { path: "/motels", element: <Motels /> },
+  { path: "/guesthouses", element: <GuestHouses /> },
+  { path: "/Luxurytents", element: <LuxuryTents /> },
+  { path: "/farmstays", element: <Farmstays /> },
+  { path: "/nyc", element: <Newyork /> },
+  { path: "/miami", element: <Miami /> },
+  { path: "/lasvegas", element: <Lasvegas /> },
+  { path: "/losangeles", element: <Losangeles /> },
+  { path: "/chicago", element: <Chicago /> },
+];
